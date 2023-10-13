9b81872e003569b1d6a75a69a5fedd13f38c09e0
ihsan <ihsann.ramdhani@gmail.com>
2023-10-13 22:04:11 +0700

    feat: change color if user click on page

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// TODO: feature to change highlight colors if user click on body
+
+// default color config
+let numColor = 1;
+
+const changeNumColor = function (num) {
+  if (num === 1) {
+    numColor++;
+    return numColor;
+  }
+  if (num === 2) {
+    numColor++;
+    return numColor;
+  }
+  if (num === 3) {
+    numColor = 1;
+    return numColor;
+  }
+};
+
+const header = document.querySelector('.header');
+const styleHighlight = document.head.appendChild(
+  document.createElement('style')
+);
+const img = document.querySelector('img');
+
+header.addEventListener('click', function (e) {
+  changeNumColor(numColor);
+  styleHighlight.innerHTML = `.highlight::after {background-color: var(--color-${numColor})}`;
+  img.src = `assets/thinking_${numColor}.svg`;
+});
