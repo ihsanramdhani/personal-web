b713c87de575551cb228996473005e070fd30850
ihsan <ihsann.ramdhani@gmail.com>
2023-10-17 22:35:47 +0700

    feat: add smooth scrolling into sections

--- script.js
+++ script.js
@@ -8,18 +8,36 @@ const changeNumColor = function (num) {
   if (num === 3) numColor = 1;
 };
 
+const containerSection = document.querySelector('.container__section');
 const header = document.querySelector('.header');
 const navHome = document.querySelector('.nav__home');
 const navProjects = document.querySelector('.nav__projects');
 const navAbout = document.querySelector('.nav__about');
+const secProjects = document.querySelector('#section__projects');
+const secAbout = document.querySelector('#section__about');
 const styleHighlight = document.head.appendChild(
   document.createElement('style')
 );
 
-header.addEventListener('click', function (e) {
+containerSection.addEventListener('click', function (e) {
   changeNumColor(numColor);
   styleHighlight.innerHTML = `
   .highlight::after {background-color: var(--color-${numColor})}
   .header__img::before {content: var(--svg-${numColor})}
   `;
+});
+
+navHome.addEventListener('click', function (e) {
+  e.preventDefault();
+  header.scrollIntoView({ behavior: 'smooth' });
+});
+
+navProjects.addEventListener('click', function (e) {
+  e.preventDefault();
+  secProjects.scrollIntoView({ behavior: 'smooth' });
+});
+
+navAbout.addEventListener('click', function (e) {
+  e.preventDefault();
+  secAbout.scrollIntoView({ behavior: 'smooth' });
 });
