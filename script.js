5de15077186b97d7e63db37b445552072eab94fb
ihsan <ihsann.ramdhani@gmail.com>
2023-10-27 15:48:35 +0700

    fix: fix conflict after rewrite

--- script.js
+++ script.js
@@ -1,74 +1,26 @@
-'use strict';
+// =============== SHOW MENU ===============
+const navMenu = document.querySelector('#nav__menu');
+const navToggle = document.querySelector('#nav__toggle');
+const navClose = document.querySelector('#nav__close');
 
-// // default color config
-// let numColor = 1;
+// MENU SHOW
+navToggle &&
+  navToggle.addEventListener('click', () => navMenu.classList.add('show-menu'));
 
-// const changeNumColor = function (num) {
-//   if (num === 1 || num === 2) numColor++;
-//   if (num === 3) numColor = 1;
-// };
+// MENU HIDDEN
+navClose &&
+  navClose.addEventListener('click', () => navMenu.classList.remove('show-menu'));
 
-// const containerSection = document.querySelector('.container__section');
-// const header = document.querySelector('.header');
-// const navHome = document.querySelector('.nav__home');
-// const navProjects = document.querySelector('.nav__projects');
-// const navAbout = document.querySelector('.nav__about');
-// const secProjects = document.querySelector('.section__projects');
-// const secAbout = document.querySelector('.section__about');
-// const styleHighlight = document.head.appendChild(
-//   document.createElement('style')
-// );
+// =============== REMOVE MENU MOBILE ===============
 
-// containerSection.addEventListener('click', function (e) {
-//   changeNumColor(numColor);
-//   styleHighlight.innerHTML = `
-//   .header__img::before {content: var(--svg-${numColor})}
-//   .highlight::after {background-color: var(--color-${numColor})}
-//   `;
-// });
+// =============== SHADOW HEADER ===============
 
-// navHome.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   header.scrollIntoView({ behavior: 'smooth' });
-// });
+// =============== EMAIL JS ===============
 
-// navProjects.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   secProjects.scrollIntoView({ behavior: 'smooth' });
-// });
+// =============== SHOW SCROLL UP ===============
 
-// navAbout.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   secAbout.scrollIntoView({ behavior: 'smooth' });
-// });
+// =============== SCROLL SECTIONS ACTIVE LINK ===============
 
-const sections = document.querySelectorAll('.section');
-const links = document.querySelectorAll('.nav--link');
+// =============== DARK LIGHT THEME ===============
 
-const displayPage = function (section) {
-  sections.forEach(section => {
-    section.style.display = 'none';
-  });
-  document.querySelector(`.${section}`).style.display = 'grid';
-};
-
-document.addEventListener('DOMContentLoaded', function () {
-  // initial load
-  if (window.location.hash) displayPage(window.location.hash.substring(1));
-  else displayPage('home');
-});
-
-// handle navigation clicks
-links.forEach(link => {
-  const page = link.getAttribute('href').substring(1);
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    displayPage(page);
-    history.pushState(null, null, `#${page}`);
-  });
-});
-
-// handle back and forward navigation
-window.addEventListener('popstate', function () {
-  displayPage(window.location.hash.substring(1) || 'home');
-});
+// =============== SCROLL REVEAL ANIMATION ===============
